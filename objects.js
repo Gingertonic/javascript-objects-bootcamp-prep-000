9b91a8c552a4d2e78c73c8ee00b0d51415760d91
Beth Schofield <bethmschofield@gmail.com>
2017-12-16 22:28:46 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -10,6 +10,6 @@ function updatePlaylist(playlist, artistName, songName) {
 }
 
 function removeFromPlaylist(playlist1, artistName){
-  delete playlist.artistName;
-  return playlist;
+  delete playlist1.artistName;
+  return playlist1;
 }
